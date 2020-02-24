fc47eaf307791bc421ba337b62885d1aab4d6285
nata2456 <you@example.com>
2020-02-24 16:46:38 +0200

    changed_t_4

--- lab5/lab5task4/lab5task4/Program.cs
+++ lab5/lab5task4/lab5task4/Program.cs
@@ -8,19 +8,16 @@ namespace lab5task4
         {
            
             Console.WriteLine("n = 10 ");
-            //int n  = Convert.ToInt32(Console.ReadLine());
-            int i = 1;
+            int n  = Convert.ToInt32(Console.ReadLine());
+            
             double x0 = 1;
-            while (i <= 10)
-            {
-                double xi;
-                xi = x0 + 1 * 2; 
+            double xi = 10;
+            for (int i = 1; i <= n; i++)
+            { 
+                xi = x0 + i * 2;
                 x0 = xi;
-                i += 1;
-                Console.WriteLine("xi = {0}", xi);
-            
             }
-            
+             Console.WriteLine("xi = {0}", xi);
 
 
         }
